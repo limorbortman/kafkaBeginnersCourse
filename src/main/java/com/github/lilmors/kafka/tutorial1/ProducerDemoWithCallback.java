885a3f0a3d6e086b3a1cb0e585e78023e8513241
Limor Stotland <limor.stotland@kenshoo.com>
2019-12-05 13:32:48 +0200

    Add producer with call back

--- src/main/java/com/github/lilmors/kafka/tutorial1/ProducerDemoWithCallback.java
+++ src/main/java/com/github/lilmors/kafka/tutorial1/ProducerDemoWithCallback.java
@@ -1,10 +1,11 @@
 package com.github.lilmors.kafka.tutorial1;
 
-import org.apache.kafka.clients.producer.KafkaProducer;
-import org.apache.kafka.clients.producer.ProducerConfig;
-import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.*;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.util.Optional;
 import java.util.Properties;
 
 /**
@@ -13,8 +14,9 @@ import java.util.Properties;
  * 2. Creating the producer
  * 3. Send the data
  */
-public class ProducerDemo {
+public class ProducerDemoWithCallback {
 
+    private static final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
     private static final String BOOTSTRAP_SERVER = "localhost:9092";
     private static final String TOPIC_NAME = "topic1";
 
@@ -30,8 +32,12 @@ public class ProducerDemo {
         KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
         //Send the data
-        ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Hi, I am here");
-        producer.send(record);
+        final ProducerRecord<String, String> record = new ProducerRecord<>(TOPIC_NAME, "Hi, I am here");
+        producer.send(record, (recordMetadata, e) -> Optional.ofNullable(e).ifPresentOrElse(
+                exception -> logger.error("Error while producing", e),
+                () -> logger.info(String.format("record for: topic = %s, partition = %d and offset = %d",
+                        recordMetadata.topic(), recordMetadata.partition(), recordMetadata.offset()))
+        ));
         //Send is async so if we want to make the producer send the data now we need to use te flush command to the close
         // for closing and flushing
         producer.close();
