0915ff4c619c5a995217dc2c254905b5c8ecb76e
Limor Stotland <limor.stotland@kenshoo.com>
2019-12-05 14:31:24 +0200

    Add consumer

---
+++ src/main/java/com/github/lilmors/kafka/tutorial1/consumer/ConsumerDemo.java
@@ -0,0 +1,53 @@
+package com.github.lilmors.kafka.tutorial1.consumer;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
+/**
+ * 4 steps when creating consumer :
+ * 1. Creating consumer properties -> https://kafka.apache.org/22/documentation.html#consumerconfigs
+ * 2. Creating the consumer
+ * 3. Subscribe the consumer to a topic(s)
+ * 4. poll for new data
+ */
+public class ConsumerDemo {
+
+    private static final String BOOTSTRAP_SERVER = "localhost:9092";
+    private static final String TOPIC_NAME = "topic1";
+    private static final String GROUP_ID = "my-group-id_1";
+
+    public static void main(String[] args) {
+
+        Logger logger = LoggerFactory.getLogger(ConsumerDemo.class);
+
+        //Creating consumer properties
+        Properties properties = new Properties();
+        properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVER);
+        properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, GROUP_ID);
+        properties.setProperty(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+
+        //Creating the consumer
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(properties);
+
+        //Subscribe the consumer to a topic
+        consumer.subscribe(Collections.singleton(TOPIC_NAME));
+
+        //Poll the data
+        //the while loop is only for the example
+        while (true) {
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(100));
+            records.forEach(record -> logger.info(String.format("Key = %s, and value = %s", record.key(), record.value())));
+        }
+    }
+}
